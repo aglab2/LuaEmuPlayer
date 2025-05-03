a1fd9e0cea213a68dd1053b25c6da33d543d736e
aglab2 <aglab3@gmail.com>
2025-05-03 18:20:55 +0800

    Attempt to fix the mislocation of stars

--- Models/GUI.cs
+++ Models/GUI.cs
@@ -197,7 +197,7 @@ namespace LuaEmuPlayer.Models
                     paint.Color = new SKColor(0xff, 0xff, 0xff);
                 }
 
-                _skiaCanvas.DrawText(message, x, y + fontSize / 2, font, paint);
+                _skiaCanvas.DrawText(message, x, y + fontSize, font, paint);
             }
             return 0;
         }
